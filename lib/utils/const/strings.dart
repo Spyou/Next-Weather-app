c5fe6446edc2bf114f80ace2395cf6e751377126
Krishna Vishwakarma <88382789+Spyou@users.noreply.github.com>
2023-08-21 19:31:16 +0530

    Update strings.dart

--- lib/utils/const/strings.dart
+++ lib/utils/const/strings.dart
@@ -1,5 +1,5 @@
 const String degree = "°";
-const String apiKey = "7f0310ae7c31380bd1052eb199cd4888";
+const String apiKey = "YOUR API KEY";
 const String currentEndpoint =
     "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
 const String daysEndpoint =
